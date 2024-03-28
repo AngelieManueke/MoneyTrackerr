0c5dd0b4f7ed0e8c89359cd6316b343a4d84983e
AngelieJosephine <angelie.m22r@gmail.com>
2024-03-28 23:54:51 +0800

    Feat : Slicing, Home, Cash on Hand, Cash On Bank

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import SplashScreen from './SplashScreen';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Home from './Home';
+import CashOnHand from './CashOnHand'
+import CashOnBank './CashOnBank';
+
+export {SplashScreen, SignIn, SignUp, Home, CashOnHand, CashOnBank};
