732e0c03e803d28f42119f2e9a0f439f6b7503f9
AngelieJosephine <angelie.m22r@gmail.com>
2024-03-29 14:30:40 +0800

    update Cash on Hand

--- src/pages/CashOnHand/index.tsx
+++ src/pages/CashOnHand/index.tsx
@@ -1,17 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { PageHeader } from '../../components'
-const CashOnHand = () => {
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {PageHeader, Gap, TextInput, Button} from '../../components';
+
+const CashonHand = ({navigation}) => {
   return (
-    <View style={styles.container}>
-    <PageHeader
-      label="Cash On Hand"
-      backButton={true}
-    />
-    </View>
-  )
-}
+    <ScrollView style={styles.container}>
+      <PageHeader
+        label="Cash On Hand"
+        backButton={true}
+        onPress={() => navigation.goBack()}
+      />
+      <Gap height={24} />
+      <View style={styles.contentWrapper}>
+        <View>
+          <TextInput label="Description" placeholder="Add the description" />
+        </View>
+        <View style={{marginTop: 17}}>
+          <TextInput label="Type" placeholder="Debit / Credit" />
+        </View>
+        <View style={{marginTop: 22}}>
+          <Button label="save" />
+        </View>
+      </View>
+      <Gap height={24} />
+      <View style={styles.contentWrapper}>
+        <Text style={styles.text}>Last 3 Transactions</Text>
+      </View>
+    </ScrollView>
+  );
+};
 
-export default CashOnHand
+export default CashonHand;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentWrapper: {
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+    height: 296,
+    paddingHorizontal: 24,
+    paddingTop: 13,
+  },
+  text: {
+    fontSize: 16,
+    fontFamily: 'Poppins-Bold',
+    color: '#000000',
+  },
+});
